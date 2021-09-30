6997ab4ae6c00e21d095d5e39af9179475f52d53
OLuchkov <47631778+OLuchkov@users.noreply.github.com>
2021-09-30 23:53:59 +0300

    fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -39,35 +39,35 @@ locals {
 
 resource "google_project_service" "run" {
   provider = google
-  project  = var.gcp_project_id  
+  project  = var.project_id  
   service = "run.googleapis.com"
   disable_on_destroy = true
 }
 
 resource "google_project_service" "iam" {
   provider = google
-  project  = var.gcp_project_id  
+  project  = var.project_id  
   service = "iam.googleapis.com"
   disable_on_destroy = true
 }
 
 resource "google_project_service" "cloudbuild" {
   provider = google
-  project  = var.gcp_project_id  
+  project  = var.project_id  
   service = "cloudbuild.googleapis.com"
   disable_on_destroy = true
 }
 
 resource "google_project_service" "datastore" {
   provider = google
-  project  = var.gcp_project_id       
+  project  = var.project_id       
   service = "datastore.googleapis.com"
   disable_on_destroy = true
 }
 
 resource "google_project_service" "enable_apigateway_service" {
   provider = google
-  project  = var.gcp_project_id
+  project  = var.project_id
   service  = "apigateway.googleapis.com"
   disable_on_destroy = true
 }
@@ -86,7 +86,7 @@ resource "google_cloud_run_service" "todoapp" {
   template {
     spec {
       containers {
-        image = "gcr.io/${var.gcp_project_id}/todoapp:latest"
+        image = "gcr.io/${var.project_id}/todoapp:latest"
         ports {
           container_port = var.server_port
         }
